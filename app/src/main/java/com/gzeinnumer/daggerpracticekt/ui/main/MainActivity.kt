f2f2c02d41ba51daf764754de2cbbfc216d78336
M. Fadli Zein <m.fadli.zein.17@gmail.com>
2020-04-29 02:37:49 +0700

    .

--- app/src/main/java/com/gzeinnumer/daggerpracticekt/ui/main/MainActivity.kt
+++ app/src/main/java/com/gzeinnumer/daggerpracticekt/ui/main/MainActivity.kt
@@ -2,6 +2,8 @@ package com.gzeinnumer.daggerpracticekt.ui.main
 
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Toast
 import com.gzeinnumer.daggerpracticekt.BaseActivity
 import com.gzeinnumer.daggerpracticekt.R
@@ -14,5 +16,17 @@ class MainActivity : BaseActivity() {
         setContentView(R.layout.activity_main)
         Log.d(TAG, "onCreate: created")
         Toast.makeText(this, "MainActivity", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.logout -> sessionManager.logOut()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
